432c1cfb6fab0e89da95bc4904b576b68fa04667
ARNAB <arnabpal2002@gmail.com>
2026-02-11 08:55:49 +0530

    inital setup

---
+++ server/src/models/job.model.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+
+const JobSchema = new Schema(
+    {
+        externalId: { type: String, required: true, index: true, unique: true },
+        title: String,
+        company: String,
+        location: String,
+        category: String,
+        source: String,
+        url: String
+    },
+    { timestamps: true }
+);
+
+export const JobModel = model("Job", JobSchema);
